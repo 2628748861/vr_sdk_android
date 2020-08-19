1312955b8117dba3e9bb72b9202d13d5bc90adf3
maixin <maixin@guoshengtianfeng.com>
2020-08-19 19:20:24 +0800

    sdk 修改

--- vr_sdk_android-master/app/src/main/java/com/wangxiaobao/vr_sdk_android_demo/MyVrActivity.java
+++ vr_sdk_android-master/app/src/main/java/com/wangxiaobao/vr_sdk_android_demo/MyVrActivity.java
@@ -17,7 +17,7 @@ public class MyVrActivity extends WxbVrActivity {
      * */
     @Override
     public void onVrInviteShareToWeChat() {
-
+        // TODO:  此处进行二维码分享处理
     }
     /**
      * VR语音房加入成功 在vr带看中能和用户正常聊天带看
@@ -31,21 +31,22 @@ public class MyVrActivity extends WxbVrActivity {
      * */
     @Override
     public void onEnterVoiceRoomFail() {
-
+        Toast.makeText(MyVrActivity.this,"VR语音房加入失败,不能与客户进行语音聊天",Toast.LENGTH_SHORT).show();
     }
     /**
-     * 进入房间进入成功  加载成功进入vr界面能进行同屏带看
+     * 房间进入成功  加载成功进入vr界面能进行同屏带看
      * */
     @Override
     public void onEnterRoomSuccess() {
 
     }
     /**
-     * 进入房间进入失败  不能进行同屏带看
+     * 房间进入失败  不能进行同屏带看
      * */
     @Override
     public void onEnterRoomFail() {
-
+        Toast.makeText(MyVrActivity.this,"房间进入失败",Toast.LENGTH_SHORT).show();
+        finish();
     }
 
     @Override
@@ -58,6 +59,7 @@ public class MyVrActivity extends WxbVrActivity {
             Toast.makeText(mContext, "跳转失败,缺少必要参数", Toast.LENGTH_SHORT).show();
             return;
         }
+        //跳转前需要进行动态相关动态权限申请
         Intent intent = new Intent(mContext, clasz);
         Bundle bundle = new Bundle();
         bundle.putParcelable("data", vrParams);
